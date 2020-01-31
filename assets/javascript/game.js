d72d3a7f65096fd2b433c6a68651c4beab8bdefb
richard mcgirt <richardmcgirt@richards-MBP.attlocal.net>
2020-01-31 10:03:54 -0500

    need images

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,68 +1,85 @@
-$(document).ready(function() {
+$(document).ready(function(){
+    var Random=Math.floor(Math.random()*101+19)
 
-    var wins= 0;
-    var losses =0;
-    var magicNumber =0;
+    $('#randomNumber').text(random);
+    var red= Math.floor(Math.random()*11+1)
+    var yellow= Math.floor(Math.random()*11+1)
+    var green= Math.floor(Math.random()*11+1)
+    var blue= Math.floor(Math.random()*11+1)
+    var userTotal=0;
+    var wins=0;
+    var losses=0;
 
-    var red= random(1, 12);
-    var blue= random(1, 12);
-    var green= random(1, 12);
-    var yellow= random(1, 12);
-
-    function random(min,max){
-        min= Math.ceil(min);
-        max=Math.floor(max);
-        return Math.floor(Math.random() * (max-min +1))+ min;
-    }
-    var randomGem = random(19, 120);
-    $("#gem").html(random);
-
+    $('#numberWins').text(wins);
+    $('#numberLosses').text(losses);
     function reset(){
-        magicNumber = 0;
-        $("magicNumber").text(magicNumber);
-        red= random(1, 12);
-        yellow= random(1, 12);
-        green= random(1, 12);
-        blue= random(1, 12);
-        randomGem= random(19, 120);
+
+        Random=Math.floor(Math.random()*101+19);
+        $('#randomNumber').text(random);
+        red= Math.floor(Math.random()*11+1);
+        yellow= Math.floor(Math.random()*11+1);
+        green= Math.floor(Math.random()*11+1);
+        blue= Math.floor(Math.random()*11+1);
+        userTotal=0;
+        $('#finalTotal').text(userTotal);
+    }
+    function winner(){
+        allert("Winner");
+        wins++;
+        $('#numberWins').text(wins);
+        reset()
+    }
+    function loser(){
+        alert ("losser");
+        losses++;
+        $('#numberLosses').text(losses);
+        reset()
     }
 
-    $('#gem').on('click', function() {
-        $("#target").text(randomGem);
+    $('#blue').one ('click', function(){
+        userTotal = userTotal + blue;
+        $('#finalTototal').text(userTotal);
+        if (userTotal === Random){
+            winner();
+        }
+        else if ( userTotal> Random){
+            loser();
+        }
     })
-    $("#red").on("click", function(){
-        magicNumber += red;
-        winLose();
-        $('#magicNumber').text(magicNumber);
-    });
-    $("#blue").on("click", function(){
-        magicNumber += blue;
-        winLose();
-        $('#magicNumber').text(magicNumber);
-    });
-    $("#yellow").on("click", function(){
-        magicNumber += yellow;
-        winLose();
-        $('#magicNumber').text(magicNumber);
-    });
-    $("#green").on("click", function(){
-        magicNumber += green;
-        winLose();
-        $('#magicNumber').text(magicNumber);
-    });
 
-    function winLose (){
-        if (magicNumber === randomGem) {
-            wins += 1;
-            alert("Winner");
-            $('#wins').text('Wins:' + wins);
-            reset();
-        }else if(magicNumber > randomGem){
-            losses += 1;
-            alert("Your a looser");
-            $('#losses').text('Losses:' + losses);
-            reset();
-        }}
+    $('#red').one ('click', function(){
+        userTotal = userTotal+ red;
+        $('#finalTotal').text(userTotal);
+        if (userTotal === Random){
+            winner();
+        }
+        else if (userTotal>random){
+            loser();
+        }
+    })
+
+    $('#yellow').one ('click', function(){
+        userTotal = userTotal+ red;
+        $('#finalTotal').text(userTotal);
+        if (userTotal === Random){
+            winner();
+        }
+        else if (userTotal>random){
+            loser();
+        }
+    })
+
+    $('#green').one ('click', function(){
+        userTotal = userTotal+ red;
+        $('#finalTotal').text(userTotal);
+        if (userTotal === Random){
+            winner();
+        }
+        else if (userTotal>random){
+            loser();
+        }
     });
+});
 
 
+    
